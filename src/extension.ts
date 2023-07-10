db563f37c725cb369bc89130ea187384b3ffe4af
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-07-10 21:54:09 +0000

    fix: removed a bug on the process of remove comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,33 @@
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('uncomment.execute', () => {
-        const editor = vscode.window.activeTextEditor;
-
-        if (editor) {
-            const document = editor.document;
-            const text = document.getText();
-
-            const noComments = removeComments(text);
-
-            editor.edit(editBuilder => {
-                const firstLine = document.lineAt(0);
-                const lastLine = document.lineAt(document.lineCount - 1);
-                const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-
-                editBuilder.replace(fullRange, noComments);
-            });
-        }
-    });
-
-    context.subscriptions.push(disposable);
-}
-
 function removeComments(text: string): string {
-    const lineComments = /\/\/.*/g;
-    const blockComments = /\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm;
-    const htmlComments = /<!--[\s\S]*?-->/gm;
-    const cssComments = /\/\*[\s\S]*?\*\//gm;
+    const lines = text.split('\n');
+    let inCommentBlock = false;
+    const cleanedLines: string[] = [];
 
-    return text
-        .replace(lineComments, '')
-        .replace(blockComments, '')
-        .replace(htmlComments, '')
-        .replace(cssComments, '');
+    for (const line of lines) {
+        let cleanedLine = line.trim();
+
+        if (inCommentBlock) {
+            if (cleanedLine.endsWith('*/')) {
+                inCommentBlock = false;
+                cleanedLine = cleanedLine.slice(0, cleanedLine.length - 2).trim();
+            } else {
+                cleanedLine = '';
+            }
+        } else {
+            if (cleanedLine.startsWith('/*')) {
+                if (cleanedLine.endsWith('*/')) {
+                    cleanedLine = cleanedLine.slice(2, cleanedLine.length - 2).trim();
+                } else {
+                    inCommentBlock = true;
+                    cleanedLine = cleanedLine.slice(2).trim();
+                }
+            } else if (cleanedLine.startsWith('//')) {
+                cleanedLine = '';
+            }
+        }
+
+        cleanedLines.push(cleanedLine);
+    }
+
+    return cleanedLines.join('\n');
 }
