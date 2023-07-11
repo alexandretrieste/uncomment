77b4e3d1db5d811b9dccdcafe1e13c6ec475410c
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-07-11 23:24:01 +0000

    Release: v1.3.9

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ ainda assim, vou vivendo as evidencias de uma louca aventura country -->
 ## Features
 - Remove comments from JavaScript, HTML, and CSS files.
 - Supports single-line and multi-line comments.
-- Removes both line comments (e.g., ) and block comments (e.g., ).
+- Removes both line comments (e.g.,`//comment` ) and block comments (e.g., `/*comment*/`).
 - Provides a command to remove comments from the active text editor.
 
 ## Usage
--- src/extension.ts
+++ src/extension.ts
@@ -7,14 +7,12 @@ export function activate(context: vscode.ExtensionContext) {
     if (editor) {
       const document = editor.document;
       const text = document.getText();
-
       const noComments = removeComments(text);
 
       editor.edit((editBuilder: vscode.TextEditorEdit) => {
         const firstLine = document.lineAt(0);
         const lastLine = document.lineAt(document.lineCount - 1);
         const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-
         const oldText = editor.document.getText(fullRange);
         const newText = oldText.replace(oldText, noComments);
 
@@ -31,12 +29,9 @@ function removeComments(text: string): string {
   const quotedRegex = /".*?"|'.*?'|`.*?`/g;
 
   let modifiedText = text;
-
-  // Declare and initialize URLs and quoted strings with their specific types
   const urls: string[] = [];
   const quoted: string[] = [];
 
-  // Replace URLs and quoted strings with placeholders
   modifiedText = modifiedText.replace(urlRegex, match => {
     urls.push(match);
     return `%%URL${urls.length - 1}%%`;
@@ -46,11 +41,8 @@ function removeComments(text: string): string {
     return `%%QUOTED${quoted.length - 1}%%`;
   });
 
-  // Remove comments
   const commentsRegex = /(\/\/.*|\/\*[\s\S]*?\*\/|=begin[\s\S]*?=end|<!--[\s\S]*?-->)/g;
   modifiedText = modifiedText.replace(commentsRegex, '');
-
-  // Replace placeholders with their original content
   modifiedText = modifiedText.replace(/%%URL(\d+)%%/g, (_, index) => urls[Number(index)]);
   modifiedText = modifiedText.replace(/%%QUOTED(\d+)%%/g, (_, index) => quoted[Number(index)]);
 
