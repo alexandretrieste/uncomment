09e4c20153dee55876fac735dadf340ff42b2640
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-07-11 22:39:33 +0000

    Release version 1.3.6

--- src/extension.ts
+++ src/extension.ts
@@ -24,48 +24,8 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function removeComments(text: string): string {
-  const lines = text.split('\n');
+  // Regex pattern to match comments that are not inside quotes or templates
+  const commentPattern = /(['"`])(?:[^\\]|\\.)*?\1|\/\/.*|\/\*[\s\S]*?\*\//g;
 
-  let insideBlockComment = false;
-  let resultLines: string[] = [];
-
-  for (const line of lines) {
-    const trimmedLine = line.trim();
-
-    if (insideBlockComment) {
-      // Check if the block comment ends in this line
-      if (trimmedLine.includes('*/')) {
-        insideBlockComment = false;
-        const remainingText = line.slice(line.indexOf('*/') + 2);
-        resultLines.push(remainingText);
-        continue;
-      }
-      continue;
-    }
-
-    let newLine = '';
-    let currentIndex = 0;
-    while (currentIndex < line.length) {
-      if (line.startsWith('//', currentIndex)) {
-        // Line comment found, skip the rest of the line
-        break;
-      } else if (line.startsWith('/*', currentIndex)) {
-        // Block comment found, check if it ends in this line
-        const endIndex = line.indexOf('*/', currentIndex + 2);
-        if (endIndex !== -1) {
-          currentIndex = endIndex + 2;
-        } else {
-          insideBlockComment = true;
-          break;
-        }
-      } else {
-        newLine += line[currentIndex];
-        currentIndex++;
-      }
-    }
-
-    resultLines.push(newLine);
-  }
-
-  return resultLines.join('\n');
+  return text.replace(commentPattern, '').replace(/^\s*[\r\n]/gm, '\n');
 }
